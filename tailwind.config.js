c1beedcb9a218db958afba4ce1605ea7c0b82ad7
Keerthi <82638167+rajitangirala@users.noreply.github.com>
2025-03-11 11:45:52 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+      "./src/*.{html,js,jsx,ts,tsx}", // Adjust the paths based on where your HTML/JSX files are located
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
